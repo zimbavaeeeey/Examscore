36b79fdcb1f997c91648b15f6568b7b3153c9f8c
zimbavaeeeey <ranjithanamika7@gmail.com>
2025-07-05 10:15:27 +0530

    Add files via upload

--- examscore.py
+++ examscore.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LinearRegression
 df=pd.read_csv("data.csv")
 x=df[['HoursStudied']]
 y=df['ExamScore']
+x_train, x_test, y_train, y_test, = train_test_split(x,y,test_size=0.2,random_state=42)
 model=LinearRegression()
 model.fit(x_train,y_train)
 st.title("ExamScore predictor")
