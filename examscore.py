d574560667c7b8510c81408a6b486c033bb5bd61
zimbavaeeeey <ranjithanamika7@gmail.com>
2025-07-05 10:05:17 +0530

    Add files via upload

---
+++ examscore.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+df=pd.read_csv("data.csv")
+x=df[['HoursStudied']]
+y=df['ExamScore']
+model=LinearRegression()
+model.fit(x_train,y_train)
+st.title("ExamScore predictor")
+st.write("Enter hours studied to predict the exam score")
+hours=st.number_imput("Hours Studied:",min_value=0.0,step=0.1)
+if st.button("Predict Score"):
+    predicted_score =model.predict([[hours]])[0]
+    st.success(f"predicted score:(predicted_score:.2f)")
+    st.write("###sample training data")
+    st.dataframe(df)
